3154ac98d9f07ca0c83286cd1e6e806f56741af5
alice <aliceworkmail@gmail.com>
2014-10-19 11:55:17 -0700

    give html

--- main.py
+++ main.py
@@ -15,11 +15,33 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+import logging
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template_values = {}
+        template = jinja_environment.get_template("home.html")
+        self.response.out.write(template.render(template_values))
+
+class GiveHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = jinja_environment.get_template("give.html")
+        self.response.out.write(template.render(template_values))
+
+class FindHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = jinja_environment.get_template("find.html")
+        self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/give', GiveHandler),
+    ('/find', FindHandler)
 ], debug=True)
