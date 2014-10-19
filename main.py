d15e870bb793ff013985002b9ab0d73b4012c186
Kevin Casey <kacasey@berkeley.edu>
2014-10-19 13:10:10 -0700

    autocomplete

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ import webapp2
 import jinja2
 import os
 import logging
+import urllib
+import urllib2
+
+import json
+
+from google.appengine.api import urlfetch
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -31,6 +37,33 @@ class MainHandler(webapp2.RequestHandler):
 class GiveHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {}
+        url = 'https://api.locu.com/v2/venue/search'
+        form_fields = {
+          "api_key" : "f165c0e560d0700288c2f70cf6b26e0c2de0348f",
+          "fields" : [ "name", "location", "contact", "categories" ],
+          "venue_queries" : [
+            {
+              "location" : {
+                "geo" : {
+                  "$in_lat_lng_radius" : [40.693134, -119.882813, 99999]
+                }
+              }
+            }
+          ]
+        }
+
+        form_data = json.dumps(form_fields)
+        json_result = urlfetch.fetch(url=url,
+            payload=form_data,
+            method=urlfetch.POST,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'})
+
+        result = json.loads(json_result.content)
+        venue_names = []
+        for venue in result['venues']:
+          venue_names.append(venue['name'].encode('ascii','ignore'))
+
+        template_values['venue_names'] = venue_names
         template = jinja_environment.get_template("give.html")
         self.response.out.write(template.render(template_values))
 
@@ -40,8 +73,42 @@ class FindHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template("find.html")
         self.response.out.write(template.render(template_values))
 
+class AutocompleteHandler(webapp2.RequestHandler):
+    def post(self):
+        url = 'https://api.locu.com/v2/venue/search'
+        form_fields = {
+          "api_key" : "f165c0e560d0700288c2f70cf6b26e0c2de0348f",
+          "fields" : [ "name", "location", "contact", "categories" ],
+          "venue_queries" : [
+            {
+              "location" : {
+                "geo" : {
+                  "$in_lat_lng_radius" : [40.693134, -119.882813, 99999]
+                }
+              }
+            }
+          ]
+        }
+
+        form_data = json.dumps(form_fields)
+        json_result = urlfetch.fetch(url=url,
+            payload=form_data,
+            method=urlfetch.POST,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'})
+
+        result = json.loads(json_result.content)
+        venue_names = []
+        for venue in result['venues']:
+          venue_names.append(venue['name'].encode('ascii','ignore'))
+
+        response = {
+          'd': venue_names,
+        }
+        self.response.out.write(json.dumps(response))
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/give', GiveHandler),
-    ('/find', FindHandler)
+    ('/find', FindHandler),
+    ('/autocomplete', AutocompleteHandler),
 ], debug=True)
